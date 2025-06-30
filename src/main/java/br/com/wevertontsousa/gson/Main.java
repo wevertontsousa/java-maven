b25a51c938c781aee60edfe7cb5e1523e19f8eda
Weverton <weverton.tsousa@hotmail.com>
2025-06-30 01:00:50 +0000

    adiciona biblioteca Gson

---
+++ src/main/java/br/com/wevertontsousa/gson/Main.java
@@ -0,0 +1,27 @@
+package br.com.wevertontsousa.gson;
+
+import com.google.gson.Gson;
+
+public class Main {
+
+  public static void main(String[] args) {
+    Foo foo = new Foo("bar", 1);
+    Gson gson = new Gson();
+    String json = gson.toJson(foo);
+    System.out.println(json);
+  }
+
+}
+
+
+@SuppressWarnings(value = "unused")
+class Foo {
+  private String bar;
+  private int baz;
+
+  public Foo(String bar, int baz) {
+    this.bar = bar;
+    this.baz = baz;
+  }
+
+}
